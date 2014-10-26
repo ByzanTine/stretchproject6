8e0e6bf5dacf87dc842b7318f1ad8635f603480a
Connor Shope <cfshope@umich.edu>
2014-10-26 11:58:58 -0400

    modified notify vol function - Connor

--- galileo/lib/touch_mapping_lib.cpp
+++ galileo/lib/touch_mapping_lib.cpp
@@ -1,20 +1,20 @@
 #include "touch_mapping_lib.h"
 
 Touch_map::Touch_map (int _pin, 
-	void (*_notify_vol)(double vol), int (*_get_val)(int pin))
+	void (*_notify_vol)(int pin, double vol), int (*_get_val)(int pin))
 	: pin(_pin), notify_vol(_notify_vol), get_val(_get_val),
 	  base_reading(0), max_reading(1023)
 	{}
 
 Touch_map::Touch_map (int _pin, 
-	void (*_notify_vol)(double vol), int (*_get_val)(int pin),
+	void (*_notify_vol)(int pin, double vol), int (*_get_val)(int pin),
 	int _base_reading)
 	: pin(_pin), notify_vol(_notify_vol), get_val(_get_val),
 	  base_reading(_base_reading), max_reading(1023)
 	{}
 
 Touch_map::Touch_map (int _pin, 
-	void (*_notify_vol)(double vol), int (*_get_val)(int pin),
+	void (*_notify_vol)(int pin, double vol), int (*_get_val)(int pin),
 	int _base_reading, int _max_reading)
 	: pin(_pin), notify_vol(_notify_vol), get_val(_get_val),
 	  base_reading(_base_reading), max_reading(_max_reading)
@@ -28,5 +28,5 @@ void Touch_map::update()
 	reading = reading > max_reading ? max_reading : reading;
 	double vol = double (reading - base_reading)
 		/ double (max_reading - base_reading);
-	notify_vol(vol);
+	notify_vol(pin, vol);
 }
--- galileo/lib/touch_mapping_lib.h
+++ galileo/lib/touch_mapping_lib.h
@@ -1,13 +1,13 @@
 class Touch_map {
 	public:
 		// constructs Touch_map using base_reading = 0; max_reading = 1023
-		Touch_map(int _pin, void (*_notify_vol)(double vol), 
+		Touch_map(int _pin, void (*_notify_vol)(int pin, double vol), 
 			int (*_get_val)(int pin));
 		// constructs Touch_map using max_reading = 1023
-		Touch_map(int _pin, void (*_notify_vol)(double vol),
+		Touch_map(int _pin, void (*_notify_vol)(int pin, double vol),
 			int (*_get_val)(int pin), int _base_reading);
 		// full constructor
-		Touch_map(int _pin, void (*_notify_vol)(double vol),
+		Touch_map(int _pin, void (*_notify_vol)(int pin, double vol),
 			int (*_get_val)(int pin), int _base_reading, int _max_reading);
 		// updates the internal state of Touch_map object; calls notify_vol
 		void update();
@@ -18,7 +18,7 @@ class Touch_map {
 		int base_reading;
 		int max_reading;
 		// callback used by update; vol < 0 is proportion of max_vol
-		void (*notify_vol)(double vol);
+		void (*notify_vol)(int pin, double vol);
 		// function used to read pin value
 		int (*get_val)(int pin);	
 };
